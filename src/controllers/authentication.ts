e5b133b4f36aa987fc67d5e58571e125c9fe8bf5
Edward Osei-Nyarko <edward.osei.nyarko@gmail.com>
2023-05-31 10:10:53 +0000

    feat: added auth controller

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import { createUser, getUserByEmail } from '../db/user'
+import { authentication, random } from 'helpers';
+
+export const register = async(req: express.Request, res: express.Response) => {
+    try {
+        const {email, password, username} = req.body;
+        if (!email || !username || !password) {
+            return res.sendStatus(400);
+        }
+        const existingUser = getUserByEmail(email);
+        if (existingUser) {
+            return res.sendStatus(400)
+        }
+        const salt = random();
+        const user = await createUser({
+            email,
+            username,
+            authentication: {
+                salt,
+                password: authentication(salt, password)
+            }
+        });
+        return res.send(200).json(user).end();
+        
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
